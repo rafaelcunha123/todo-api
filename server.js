da7523e3837ee54f5566ef234bf7595a82c2659f
Rafael Cunha <rafaelcunha13@gmail.com>
2016-08-15 20:36:10 -0300

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -45,8 +45,34 @@ app.delete('/todos/:id', function (req, res) {
 		todos = _.without(todos, matchTodo);
 		res.status(200).json(matchTodo);
 	};
+});
 
-})
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchTodo) {
+		return res.status(404).send();
+	};
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	};
+
+	if (body.hasOwnProperty('description') && _.isString(body.completed) && body.completed.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	};
+
+	_.extend(matchTodo, validAttributes);
+	res.json(matchTodo);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
