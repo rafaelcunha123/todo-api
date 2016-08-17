822880eebcf99fc40e332dec1054af35e81fe55c
Rafael Cunha <rafaelcunha13@gmail.com>
2016-08-17 19:08:40 -0300

    Added DELETE /users/login

--- middleware.js
+++ middleware.js
@@ -1,13 +1,34 @@
+var crypto = require('crypto-js')
 module.exports = function (db){
 	return {
 		requireAuthentication: function(req, res, next){
-			var token = req.get('Auth');
-			db.user.findByToken(token).then(function(user){
+			var token = req.get('Auth') || '';
+
+			db.token.findOne({
+				where:{
+					tokenHash: crypto.MD5(token).toString()
+				}
+			}).then(function(tokenInstance){
+				if (!tokenInstance) {
+					throw new Error ();
+				}
+				req.token = tokenInstance;
+				return db.user.findByToken(token)
+			}).then(function(user){
+				req.user = user;
+				next();
+			})
+			.catch(function(e){
+				res.status(401).send();
+			})
+
+
+			/*db.user.findByToken(token).then(function(user){
 				req.user = user;
 				next();
 			}, function(){
 				res.status(401).send();
-			})
+			})*/
 		}
 	};
 };
--- server.js
+++ server.js
@@ -145,16 +145,28 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
-		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
+			var token = user.generateToken('authentication');
+			userInstance = user;
+
+			return db.token.create({
+				token: token
+			})
+		}).then(function(tokenInstance){
+			res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+		})
+		.catch(function(e) {
 			res.status(401).send();
-		}
-	}, function(e) {
-		res.status(401).send();
+		});
+});
+
+app.delete('/users/login', middleware.requireAuthentication,function(req, res){
+	req.token.destroy().then(function(){
+		res.status(204).send();
+	}).catch(function(e){
+		res.status(500).send();
 	});
 });
 
