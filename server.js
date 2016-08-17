13929551265ec8568a99dc9ea41cba002e1064ce
Rafael Cunha <rafaelcunha13@gmail.com>
2016-08-17 16:36:08 -0300

    Add auth token generation to POST /users/login

--- server.js
+++ server.js
@@ -130,7 +130,12 @@ app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON());
+		var token = user.generateToken('authentication');
+		if(token){
+			res.header('Auth', token).json(user.toPublicJSON());	
+		} else {
+			res.status(401).send();	
+		}
 	}, function(e){
 		res.status(401).send();
 	});
@@ -138,7 +143,7 @@ app.post('/users/login', function(req, res) {
 
 
 db.sequelize.sync({
-	force: true
+	//force: true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
